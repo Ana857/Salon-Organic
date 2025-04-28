51d5f0f3f46d7ee2cc2bc24abe8e25d84cc5c5e4
Ana857 <ciupercataiata@gmail.com>
2025-04-28 09:10:26 +0300

    style commit

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function setUp(){
-    document.getElementById(".link_taskbabr").onclick = setServices;
+let button=document.querySelector(".mobile_menu")
+let list=document.querySelector(".list_taskbar")
+
+button.addEventListener("click", list_show)
+function list_show () {
+    list.classList.toggle("show_list_mobile")
 }
-function setServices(){
-    let menuServices = document.getElementById("menu-services")
-    menuServices.className = "menuSevices"
+
+let plus_btn=document.querySelector(".plus_icon")
+let list_mobile=document.querySelector(".list_services")
+
+plus_btn.addEventListener( "click" , icon_show )
+function icon_show () {
+    list_mobile.classList.toggle("show_services_list_mobile")
+    console.log(1)
 }
-setUp()
